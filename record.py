883ee783c0a021fde1aa071413b4a3670e4879e1
jasonrobwebster <jasonrobwebster@gmail.com>
2019-01-01 10:30:05 +0200

    minor fixes

--- record.py
+++ record.py
@@ -1,8 +1,14 @@
 from gamepy import Recorder
 from pynput.keyboard import KeyCode, Key
 
-rec = Recorder(['w','a','s','d'])
+pause_keys='pause'
+terminate_keys='end'
+start_keys='home'
+
+rec = Recorder(['w','a','s','d'], pause_keys=pause_keys, terminate_keys=terminate_keys, start_keys=start_keys)
+
+print('Press %s to start the recording, %s to pause the recording, and %s to terminate the recording.' %(str(start_keys), str(pause_keys), str(terminate_keys)))
 
 small_bbox = [50,161,850,550]
-large_bbox = [0,200,1440,900]
+large_bbox = [0,0,1920,1024]
 rec.record('./test/image.png', bbox = large_bbox)
